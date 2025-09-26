833ec3726842515ef2d51775815d970b751975bd
hanifalfaiz <stshfaiz@gmail.com>
2025-09-26 20:00:19 +0700

    Update laprak1.cpp

--- week1/laprak1.cpp
+++ week1/laprak1.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
 using namespace std;
-void Kalkulator() {
-    int a, b;
-    char op;
-    cout << "Masukkan angka pertama: ";
-    cin >> a;
-    cout << "Masukkan operator (+, -, *, /): ";
-    cin >> op;
-    cout << "Masukkan angka kedua: ";
-    cin >> b;
-    switch (op) {  
-        case '+':
-            cout << "Hasil: " << a + b << endl;
-            break;
-        case '-':
-            cout << "Hasil: " << a - b << endl;
-            break;
-        case '*':
-            cout << "Hasil: " << a * b << endl;
-            break;
-        case '/':
-            if (b != 0) {
-                cout << "Hasil: " << a / b << endl;
-            } else {
-                cout << "Error: pembagian dengan 0 tidak terdefinisi" << endl;
-            }
-            break;
-        default:
-            cout << "Operator tidak valid!" << endl;
-            break;
-        }
+void mesin(){
+    float a,b;
+    cout<<"Masukkan angka pertama: ";
+    cin>>a;
+    cout<<"Masukkan angka kedua: ";
+    cin>>b;
+
+    //penjumlahan
+    cout<<"penjumlahan: "<<a+b<<endl;
+    //pengurangan
+    cout<<"pengurangan: "<<a-b<<endl;
+    //perkalian
+    cout<<"perkalian: "<<a*b<<endl;
+    //pembagian
+    if (b!=0){
+        cout<<"pembagian: "<<a/b<<endl;
+    } else {
+        cout<<"pembagian: tidak terdefinisi (pembagi nol)"<<endl;
+    }
 }
-int main() {
-    Kalkulator();
+int main(){
+    mesin();
     return 0;
 }
